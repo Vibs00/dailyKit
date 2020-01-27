a945915968e075574e51dbdc3794dbf76de387f3
Vibs00 <vibha.ajm13@gmail.com>
2020-01-27 10:39:55 +0530

    api added

--- src/table.js
+++ src/table.js
@@ -1,17 +1,22 @@
 import React from 'react';
 import Card from 'react-bootstrap/Card';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import axios from 'axios';
 
 class Row extends React.Component {
+  constructor(props) {
+    super(props);
+  }
   render() {
     return (
-      <Card bg="secondary" text="white" style={{ width: '18rem' }}>
-        <Card.Header>Header</Card.Header>
+      <Card bg="secondary" text="white" style={{ width: '18rem', margin: "1rem"}}>
+        <Card.Header>{this.props.gender}</Card.Header>
         <Card.Body>
-          <Card.Title>Secondary Card Title</Card.Title>
+          <Card.Title>{this.props.name}</Card.Title>
           <Card.Text>
-            Some quick example text to build on the card title and make up the bulk
-            of the card's content.
+            Birth Year: {this.props.birth_year}<br />
+            Homeword: {this.props.homeword}<br />
+            Height: {this.props.height}<br />
           </Card.Text>
         </Card.Body>
       </Card>
@@ -22,13 +27,38 @@ class Row extends React.Component {
 class Table extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      info: [' ']
+    }
   }
 
+  addRow = () => {
+    let info = this.state.info.slice();
+    let rows = info.map((comp) => <Row name={comp.name} gender={comp.gender} height={comp.height} homeword={comp.homeword} birth_year={comp.birth_year} />);
+    return rows;
+  }
 
-  
+  componentWillMount() {
+    let info;
+    const url = "https://swapi.co/api/people";
+    axios.get(url)
+      .then(function (response) {
+        console.log(response);
+        info = response.data.results.slice();
+      })
+      .then(() => {
+        this.setState({
+          info: info
+        })
+        console.log(this.state.info);
+      })
+  }
+
   render() {
     return (
-      <Row />
+      <>
+        {this.addRow()}
+      </>
     );
   }
 }
